d3b76f59a2e855aa4719e4e6910c7538485da07d
Andriikryl <andreyk557@gmail.com>
2024-05-27 18:14:21 +0300

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+import React, { useEffect } from "react";
+import Hero from "@/components/hero/Hero";
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+import Container from "@/components/container/Container";
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+
 export default function Home() {
+  const color = useMotionValue(COLORS_TOP[0]);
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, [color]);
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
   return (
-    <div>
-      <h1>asd</h1>
-    </div>
+    <motion.section
+      style={{
+        backgroundImage,
+      }}
+      className="full"
+    >
+      <Container>
+        <Hero />
+      </Container>
+    </motion.section>
   );
 }
